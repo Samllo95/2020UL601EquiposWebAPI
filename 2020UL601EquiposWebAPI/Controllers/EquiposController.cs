060ac54e6afeafd6bf9da4993b3b98e621653274
Samllo95 <samllo95@hotmail.com>
2022-02-26 02:26:13 -0600

    Primera Copia

---
+++ 2020UL601EquiposWebAPI/Controllers/EquiposController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using _2020UL601EquiposWebAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace _2020UL601EquiposWebAPI.Controllers
+{
+    //[Route("api/[controller]")]
+    [ApiController]
+    public class EquiposController : ControllerBase
+    {
+        private readonly PrestamosContext _contexto;
+
+        public EquiposController(PrestamosContext mycontext)
+        {
+            this._contexto=mycontext;
+        }
+
+        [HttpGet]
+        [Route("api/equipos")]
+        public IActionResult Get()
+        {
+            IEnumerable<Equipos> equiplist = from e in _contexto.equipos select e;
+
+            if (equiplist.Count()>0)
+            {
+                return Ok(equiplist);
+            }
+
+            return NotFound();
+        }
+
+        //[HttpGet]
+        //[Route("api/equipos/{idequipo}")]
+        //public IActionResult Get(int idequipo)
+        //{
+
+        //    Equipos equiplist = (from e in _contexto.equipos where e.id_equipos==idequipo select e).FirstOrDefault();
+
+        //    if (equiplist!=null)
+        //    {
+        //        return Ok(equiplist);
+        //    }
+
+        //    return NotFound();
+        //}
+
+    }
+}
---
+++ 2020UL601EquiposWebAPI/PrestamosContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using _2020UL601EquiposWebAPI.Models;
+
+namespace _2020UL601EquiposWebAPI
+{
+    public class PrestamosContext:DbContext
+    {
+        public PrestamosContext(DbContextOptions<PrestamosContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Equipos> equipos { get; set; }
+    }
+}
